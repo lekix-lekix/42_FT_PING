bd18613381c33cdf2a77acb2dd10b4e8feae0c52
Kilian Pouliquen <kipouliq@f6r13s12.clusters.42paris.fr>
2026-03-10 15:12:20 +0100

    added vagrant + setting up socket

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,29 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "generic/debian12"
 
+  config.vm.synced_folder "~/.ssh", "/home/vagrant/.ssh_host", type: "virtualbox"
+
   config.vm.define "ft_ping_vm" do |ft_ping_vm|
     ft_ping_vm.vm.provider "virtualbox"
 
-    server.vm.provision "shell", inline: <<-SHELL
+    ft_ping_vm.vm.provision "shell", inline: <<-SHELL
         sudo apt update
-        sudo apt-install inetutils-ping -y 
+        sudo apt-get install inetutils-ping -y
+
+        # Copie les clés SSH de l'hôte
+        cp /home/vagrant/.ssh_host/id_rsa ~/.ssh/id_rsa 2>/dev/null || \
+        cp /home/vagrant/.ssh_host/id_ed25519 ~/.ssh/id_ed25519 2>/dev/null
+
+        chmod 600 ~/.ssh/id_*
+
+        # Config Git
+        # git config --global user.email "ton@email.com"
+        # git config --global user.name "Ton Nom"
+
+        # Accepte github.com sans prompt
+        ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null
+
+        git clone git@github.com:lekix-lekix/42_FT_PING.git ft_ping
     SHELL
   end
 end
--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -6,15 +6,16 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 15:41:53 by kipouliq          #+#    #+#             */
-/*   Updated: 2026/03/09 16:40:44 by kipouliq         ###   ########.fr       */
+/*   Updated: 2026/03/10 14:56:11 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// 1. Parse arguments
-// 2. Resolve hostname (if necessary) && init raw socket
-// 3. Construct custom IMCP header
-// 4. Wrap it in ip header (automatic ?)
+// 1. Parse arguments  -> done
+// 2. Resolve hostname (if necessary)  -> done
+// -. Setup RAW Socket
 // === PING LOOP ===
+// 3. Construct ip + IMCP header
+// 4. Wrap it in ip header (automatic ?)
 // 4. Start timer and send packet
 // 5. Receive packet and calculate time
 // 6. Print it
@@ -22,6 +23,24 @@
 // 7. Handle signals for ctrl c (and sigalarm ?)
 
 #include "../ft_ping.h"
+
+void	setup_socket(int *sock, struct addrinfo *res_list)
+{
+	for (struct addrinfo *curr = res_list; curr != NULL; curr = curr->ai_next)
+	{
+		*sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+		if (*sock == -1)
+			continue ;
+		if (*sock)
+			break ;
+	}
+	if (*sock == -1)
+	{
+		perror("socket");
+		freeaddrinfo(res_list);
+		exit(-1);
+	}
+}
 
 void	resolve_host(char *host, struct addrinfo **res)
 {
@@ -32,11 +51,12 @@ void	resolve_host(char *host, struct addrinfo **res)
 	hints.ai_family = AF_INET; // ipv4
 	hints.ai_socktype = SOCK_RAW;
 	err = getaddrinfo(host, NULL, &hints, &(*res));
-	if (err != 0)
+	if (err != 0) // handle other cases
 	{
 		if (err == -2)
 		{
 			dprintf(STDERR, "ping: unknown host\n");
+			freeaddrinfo(*res);
 			exit(1);
 		}
 	}
@@ -46,6 +66,7 @@ int	main(int argc, char **argv)
 {
 	struct addrinfo *res;
 	char			*host;
+	int				socket;
 
 	if (argc < 2) // Will change if bonuses implemented
 	{
@@ -57,4 +78,6 @@ int	main(int argc, char **argv)
 	host = NULL;
 	parse_args(argv + 1, &host);
 	resolve_host(host, &res);
+	setup_socket(&socket, res);
+	freeaddrinfo(res);
 }
